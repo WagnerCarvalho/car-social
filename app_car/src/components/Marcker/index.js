726b4a8a33ac6511575f6e67b33764e18498b11c
Wagner Carvalho <wagner.carvalho@rappi.com>
2020-05-02 15:53:24 -0300

    render maps with marker

---
+++ app_car/src/components/Marcker/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Marker } from 'react-native-maps'
+import { Image, View } from 'react-native'
+
+function MarkerCarSocial(chauffeur) {
+    return(
+        <>
+            {chauffeur.data.map((items)=>{
+                return(
+                    <Marker
+                        coordinate={items.coords}
+                        title={items.title}
+                        description={items.description}>
+                        <Image source={items.profile === 'user'? require('../../img/user.png'): require('../../img/car.png')} style={{height:40, width:20}} />
+                    </Marker>
+                )
+            })}     
+        </>
+    )
+}
+
+export default MarkerCarSocial
---
+++ app_car/src/pages/Ride/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Background = styled.View`
+    flex: 1;
+`
+
+export const Container = styled.View`
+    flex: 1;
+`
