297297357fc90654e8548610fca53779c3e22349
Wagner Carvalho <wagner.carvalho@rappi.com>
2020-05-07 10:30:19 -0300

    config firebase

--- app_car/src/services/firebaseConections.js
+++ app_car/src/services/firebaseConections.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 let config = {
-    apiKey: "AIzaSyAhJk2W5BXij63OGuFu2wLVLEzMKnMz0Fs",
+    apiKey: "${apiKey}",
     authDomain: "carsocial.firebaseapp.com",
     databaseURL: "https://carsocial.firebaseio.com",
     projectId: "carsocial",
     storageBucket: "carsocial.appspot.com",
-    messagingSenderId: "838391032294",
-    appId: "1:838391032294:web:3ad0b3e50b8491f15e0e27"
+    messagingSenderId: "${messagingSenderId}",
+    appId: "${appId}"
   };
   
   firebase.initializeApp(config);
