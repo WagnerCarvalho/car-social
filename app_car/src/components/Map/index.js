d6359bed9d9ac6f0527cb83ff96354e87fe62971
Wagner Carvalho <wagner.carvalho@rappi.com>
2020-05-10 10:46:55 -0300

    render markers

--- app_car/src/components/Map/index.js
+++ app_car/src/components/Map/index.js
@@ -10,24 +10,22 @@ function MapRender(coords) {
     const [chauffeur, chauffeurSet] = useState(origin)
     const [renderMap, setRenderMap] = useState(false)
     
-    function getChauffeur() {
-        origin.push(
-            {key:origin.length+1, coords:{latitude: -23.550055011589965, longitude:-46.58256232738495}, title:"Motorista Feliz", description:"Gol 1.6 - Placa OHL 2010", profile:"chauffeur"},
-            {key:origin.length+2, coords:{latitude: -23.542083917796194, longitude:-46.58601097762585}, title:"Motorista com Reliz", description:"Uno 1.0 - Placa FBC 3422", profile:"chauffeur"},
-            {key:origin.length+3, coords:{latitude: -23.554676881238755, longitude:-46.58105693757534}, title:"Motorista com Triste", description:"Golf 2.0 - Placa GFE 0022", profile:"chauffeur"},
-            {key:origin.length+4, coords:{latitude: -23.54350395985448, longitude:-46.577389016747475}, title:"Motorista com Triste", description:"Satana 2.0 - Placa REJ 2072", profile:"chauffeur"},
-            {key:origin.length+5, coords:{latitude: -23.554676881238755, longitude:-46.586420349776745}, title:"Motorista com Triste", description:"KA 2.0 - Placa GGE 0342", profile:"chauffeur"},
-            {key:origin.length+6, coords:{latitude: -23.54670606768003, longitude:-46.58456426113844}, title:"Motorista com Triste", description:"Fiesta 2.0 - Placa IFM 1234", profile:"chauffeur"}
-        )
-        chauffeurSet(origin)
-    }
+    async function getChauffeur() {
+        await api.get(`/rides/v1/near?distance=5000&latitude=${coords.data.latitude}&longitude=${coords.data.longitude}`)
+        .then((res)=>{
+            res.data.map((item)=>{
+                let car = {key: item.id, coords:{latitude: item.coords.latitude, longitude: item.coords.longitude}, name: item.name, description: `${item.car.model} - ${item.car.plate}`, profile:"chauffeur", recepient: coords }
+                origin.push(car)
+            })
+            chauffeurSet(origin)
+        })
+        .catch((err)=>{alert(err)})
+    }    
 
     useEffect(()=>{
         if (coords.data !== null) {
             getChauffeur()
             setRenderMap(true)
-            console.log(coords.data.latitude)
-            console.log(coords.data.longitude)
         }
     },[coords])
 
