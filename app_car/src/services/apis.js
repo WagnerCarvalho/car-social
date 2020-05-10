8ca020d94c155b06f2f1cf2a7c2bfe79046d7585
Wagner Carvalho <wagner.carvalho@rappi.com>
2020-05-10 17:48:38 -0300

    add readme

--- app_car/src/services/apis.js
+++ app_car/src/services/apis.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://54.86.106.171/'
+    baseURL: 'http://XX.XX.XXX.XXX/'
 })
 
 export default api
--- app_car/src/services/firebaseConections.js
+++ app_car/src/services/firebaseConections.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 let config = {
-    apiKey: "XXXX",
-    authDomain: "XXXX",
-    databaseURL: "XXXX",
-    projectId: "XXXX",
-    storageBucket: "XXXX",
-    messagingSenderId: "XXX",
-    appId: "XXXX"
-  };
+  apiKey: "XXXX",
+  authDomain: "XXXX",
+  databaseURL: "XXXX",
+  projectId: "XXXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
   
   firebase.initializeApp(config);
 
